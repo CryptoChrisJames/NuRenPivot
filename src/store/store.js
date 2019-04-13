6ff4b44fcad554dccb4ea3838e30a5fb92b14f2b
Christopher Smith <csmith4work@gmail.com>
2019-04-13 17:57:08 -0500

    adding static files and putting links in store.

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state:{
+        videos:[
+            { name: "The 'L' Word", link:"https://www.youtube.com/watch?v=ytJOKMdWgzU"},
+            { name: "I'm In Your Body", link:"https://www.youtube.com/watch?v=Rb2Z271fuuA"},
+            { name: "Good Vibes Music Video", link:"https://www.youtube.com/watch?v=4jABhS5gkVk"},
+            { name: "Arreic Summer Soul Funk Jam", link:"https://www.youtube.com/watch?v=33c-dTbLU6U"},
+            { name: "Nefi Promo Video", link:"https://www.youtube.com/watch?v=4nhfKVNGj1A"}
+        ]
+    }
+})
