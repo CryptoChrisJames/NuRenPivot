ea850bf1025224b8dc7e7b0690c7b97449898de9
Christopher Smith <csmith4work@gmail.com>
2019-05-17 18:23:51 -0500

    created more elaborate build script that checks for existing images

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
 sudo -i
-docker build -t "nurenui-v1" .
+
+# Initializing Variables
+IMAGETAG = nurenui-v1
+
+# Initializing functions
+buildImage()
+{    
+    docker build -t "$IMAGETAG" .
+}
+
+removeOldImage()
+{
+    docker rmi nurenui-v1
+}
+
+
+
+if [[ "$(docker images $IMAGETAG)" == "" ]]; then
+    buildImage
+else
+    removeOldImage
+    buildImage
+fi
