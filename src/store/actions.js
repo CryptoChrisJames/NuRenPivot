baf6f3021c00cb0a45d6c05cf9584f6d1a2f3637
Christopher Smith <csmith4work@gmail.com>
2019-11-29 22:57:38 -0600

    Solved concurrent API issue.

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,25 +9,13 @@ const projectsRoute = "/project/";
 const getVideoListandObjects = async ({commit}) => {
   const videoKeysUrl = apiUrl + projectsRoute + "videos";
   const keys = await axios.get(videoKeysUrl);
-  const videoObjectUrls = [];
-  for (let i = 0; i < keys.data.length; i ++) {
-    videoObjectUrls.push(apiUrl + projectsRoute + `thumbnails/${keys.data[i]}`)
-  }
-  const videoObjects = videoObjectUrls.map( async video => {
+  const videoObjectUrls = keys.data.map(key => {
+    return apiUrl + projectsRoute + `thumbnails/${key}`;
+  });
+  const videoObjects = await Promise.all(videoObjectUrls.map( async video => {
     const response = await axios.get(video);
     return response.data;
-  })
-  // Promise.all(videoObjectUrls.map( async video => {
-  //   return await axios.get(video)
-  //     .then(response => {
-  //       console.log(response.data);
-  //       //videoObjects.push(response.data);
-  //     })
-  // }))
-  // for (let i = 0; i < videoObjectUrls.length; i ++) {
-  //   const obj = await axios.get(videoObjectUrls[i]);
-  //   videoObjects.push(obj.data);
-  // }
+  }));
   const videoPayload = {
     keys: keys.data,
     videoObjects: videoObjects,
