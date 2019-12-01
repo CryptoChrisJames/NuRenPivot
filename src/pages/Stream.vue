b793b9b4d29507d1d5c37f61961b87ad2f781fbb
Christopher Smith <csmith4work@gmail.com>
2019-12-01 12:23:19 -0600

    WIP

--- src/pages/Stream.vue
+++ src/pages/Stream.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    
+    <section-head>{{ currentVideo.name }}</section-head>
+    <h3>{{ currentVideo.description }}</h3>
   </div>
 </template>
 
 <script>
+import sectionHead from '../components/atoms/Header/SectionHead.vue';
+import gallery from '../components/molecules/Gallery/Gallery.vue';
 export default {
   name: 'Stream',
-  mounted() {
-  }
+  components: { sectionHead, gallery },
+  computed: {
+    currentVideo() {
+      return this.$route.params.video;
+    },
+  },
 };
 </script>
 
