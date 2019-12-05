a6822109e1553915257d07cb3cf10d778626a091
Christopher Smith <csmith4work@gmail.com>
2019-12-05 17:03:26 -0600

    Finishing About page.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,6 +3,12 @@
     <img src="../assets/About.jpg" class="pic">
     <div class="description">
       <section-head>About</section-head>
+      <h3 class="desc">
+        Nu Renaissance takes pride in offering opportunities to a diverse group of people.
+        The greatest asset that Nu Renaissance provides is a great, positive and professional
+        outlook on the craft of media produciton. We believe that inclusion and diversity is integral
+         to our success.
+      </h3>
     </div>
   </div>
 </template>
@@ -17,7 +23,7 @@ export default {
 
 <style lang="scss" scoped>
 .aboutContainer {
-  padding: 0;
+  padding: 25px 75px;
   margin: 0;
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -30,5 +36,9 @@ export default {
 
 .description {
   text-align: center;
+}
+
+.desc {
+  padding: 25px;
 }
 </style>
