28bc6d8c42e6f3ca05888abb1dc81146c13a8a6c
Chris Smith <chris.smith@smiledirectclub.com>
2020-07-27 09:27:30 -0500

    more WIP.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,6 @@
   <div class="content">
     <section-head>About Us</section-head>
     <div class="aboutContainer">
-      <img src="../assets/Chan.gif" class="pic">
       <div class="description">
         <h3 class="desc">
           Nu Renaissance takes pride in offering opportunities to a diverse group of people.
@@ -10,6 +9,25 @@
           outlook on the craft of media produciton. We believe that inclusion and diversity is integral
           to our success.
         </h3>
+        <section-head>The Team</section-head>
+        <div class="teamMemeberWrapper">
+          <div class="teamMember">
+            <h5></h5>
+            <div class="teamMemeberGif">
+              <img src="../assets/Chan.gif" class="gif">
+            </div>
+            <div class="position"><h6></h6></div>
+          </div>
+        </div>
+        <div class="teamMemeberWrapper">
+          <h5></h5>
+          <div class="teamMember">
+            <div class="teamMemeberGif">
+              <img src="../assets/Chan.gif" class="gif">
+            </div>
+            <div class="position"><h6></h6></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -50,7 +68,7 @@ export default {
   }
 }
 
-.pic {
+.gif {
   margin: 0 auto;
   width: 500px;
   height: auto;
@@ -81,6 +99,7 @@ export default {
 }
 
 .desc {
+  font-size: 18px;
   padding: 25px;
   justify-content: left;
   font-family: 'Montserrat', sans-serif;
