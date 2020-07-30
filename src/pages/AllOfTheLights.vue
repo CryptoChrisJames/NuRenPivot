589f54b92c62b31c406136810c03a0d5df5fdc27
Christopher Smith <csmith4work@gmail.com>
2020-07-30 14:06:27 -0500

    wip.

--- src/pages/AllOfTheLights.vue
+++ src/pages/AllOfTheLights.vue
@@ -1,14 +1,26 @@
 <template>
-    <section class="titlePoster">
-        <img :src="getContent('AOTLTitle.jpg')" alt="" class="title">
-    </section>
+    <div class="AOTL">
+        <section class="landing">
+            <div class="titlePoster">
+                <img :src="getContent('AOTLTitle.jpg')" alt="" class="title">
+                <div class="navWrapper">
+                    <navbar />
+                </div>
+            </div>
+            <p>test</p>
+        </section>
+    </div>
 </template>
 
 <script>
 import config from '../../config.js';
+import navbar from '../components/atoms/Header/NavigationBar.vue';
 
 export default {
     name: "AllOfTheLights",
+    components: {
+        navbar,
+    },
     methods: {
         getContent(key) {
             return this.getAPIUrl() + key;
@@ -24,7 +36,20 @@ export default {
 @import '../styles/_colors.scss';
 @import '../styles/_variables.scss';
 
-.title{
-    max-width: 100%;
+.titlePoster {
+    position: relative;
+    min-width: 100%;
+    min-height: 100%;
+}
+
+.title {
+    position: absolute;
+    width: 100%;
+}
+
+.navWrapper {
+    position: absolute;
+    padding: 15px;
+    width: 100%;
 }
 </style>
