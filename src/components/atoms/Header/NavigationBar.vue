339ad4d1f4926a85de5adbb7f41405063e3905ac
Christopher Smith <csmith4work@gmail.com>
2020-08-10 17:49:46 -0500

    finished menu

--- src/components/atoms/Header/NavigationBar.vue
+++ src/components/atoms/Header/NavigationBar.vue
@@ -6,11 +6,32 @@
       :class="['fa fa-bars', isMobile, 'menu']"
     />
     <div v-if="menuOpen" class="menuScreen">
-      <i
-        @click="toggleMenu()"
-        :class="['fa fa-times', isMobile, 'close']"
-      >
-      </i>
+      <div class="menuScreenContent">
+        <div class="menuHeaderWrapper">
+          <a href="/"><img src="../../../assets/logo-trans2.png" alt="" class="logo"></a>
+          <i
+            @click="toggleMenu()"
+            :class="['fa fa-times', isMobile, 'close']"
+          >
+          </i>
+        </div>
+        <div class="menuOptionsWrapper">
+          <div class="menuOptions">
+            <div class="option">
+              <router-link class="menuOption" to="/"> <b>HOME</b> </router-link>
+            </div>
+            <div class="option">
+              <router-link class="menuOption" to="allofthelights"> <b>ALL OF THE LIGHTS</b> </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="socialWrapper">
+          <a href="https://www.facebook.com/AOTLShort"><i class="social fab fa-facebook-square"></i></a>
+          <a href="https://www.instagram.com/nu_ren_productions/"><i class="social fab fa-instagram"></i></a>
+          <a href="https://vimeo.com/chandonae"><i class="social fab fa-vimeo-v"></i></a>
+          <a href="https://www.youtube.com/channel/UCbMEhrBBQ4jTiC7R3RwSMWA"><i class="social fab fa-youtube"></i></a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +50,7 @@ export default {
       return this.windowWidth <= 766;
     },
     isMobile(){
-      return this.mobile ? ' fa-2x ' : ' fa-3x';
+      return this.mobile ? ' fa-2x ' : ' fa-3x ';
     }
   },
   async mounted() {
@@ -90,16 +111,61 @@ export default {
   z-index: 100;
 }
 
-.closeWrapper {
-  height: 77px;
+.menuHeaderWrapper {
+  width: 100%;
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  padding: 25px;
+}
+
+.menuOptionsWrapper {
+  padding-top: 50px;
+  color: $AOTLYellow;
+}
+
+.menuScreenContent {
+  display: block;
+  margin: 0 auto;
+  height: 100%;
   width: 100%;
 }
 
+.menuOption {
+  text-decoration: none;
+}
+
+.menuOption:hover {
+  color: $AOTLDarkYellow;
+}
+
 .close {
-  width: 100%;
   color: white;
-  padding-top: 77px;
+  text-shadow: none;
+}
+
+.option {
+  text-align: center;
+  padding: 35px;
+  font-size: 2rem;
+  line-height: 1.4;
+}
+
+.socialWrapper {
   display: flex;
+  padding: 3rem 0px;
   justify-content: center !important;
+}
+
+.social {
+  padding: 0px 12px;
+  font-size: 25px;
+  color: $AOTLYellow;
+}
+
+.social:hover {
+  color: $AOTLDarkYellow;
 }
 </style>
