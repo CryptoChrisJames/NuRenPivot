1c73ef8ce76717e64b6c552ccbaf2ca81b72c1a5
Christopher Smith <christopherjamessmithus@gmail.com>
2021-01-04 12:16:02 -0600

    huh???

--- siteV3/pages/index.vue
+++ siteV3/pages/index.vue
@@ -80,7 +80,6 @@
         </div>
       </div>
       <section-head>Creative Portfolio</section-head>
-      <gallery></gallery>
     </section>
     <p style="justify-content: center; text-align: center;">
       Icons made by <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
